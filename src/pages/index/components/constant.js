1618544ed81f131059e65b6059c5fe72599edb77
tangxd5 <tangxd5@asiainfo.com>
2025-04-06 00:10:49 +0800

    更新密码校验，增加临时密码

---
+++ src/pages/index/components/constant.js
@@ -0,0 +1,20 @@
+export const PermanentPassword = 'dld2077'
+
+export const TemporaryPassword = [
+  { 
+    value: 'lol2077',
+    time: '1'
+  },
+  { 
+    value: 'cf2035',
+    time: '3'
+  },
+  { 
+    value: 'dnf2025',
+    time: '10'
+  },
+  { 
+    value: 'dota2',
+    time: '30'
+  },
+]
